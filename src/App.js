3b1439b53f095fb33b44b658869ea1a9edec3fe6
Abhishek shaw <76241903+abhi-shek-02@users.noreply.github.com>
2022-07-19 01:52:30 +0530

    Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
-
+// 
 function App() {
   const [total, setTotal] = useState(0)
   const [expense, setexpense] = useState(0)
@@ -9,45 +9,45 @@ function App() {
 
 
   const [amount, setAmount] = useState(0)
-  const [date, setDate] = useState()
-  const [valx, setValx] = useState()
+  const [date, setDate] = useState('')
+  const [valx, setValx] = useState('')
   const [list, setList] = useState([])
-  const [OBlist, setOBList] = useState({})
 
+  const [startIndex, setStartIndex] = useState(1)
+  const [endIndex, setEndIndex] = useState(10)
 
-  const storeAmount = (e) => {
-    setAmount(e.target.value)
-  }
-  const storeDate = (e) => {
-    setDate(e.target.value)
-  }
   const storevalue = (e) => {
     setValx(e.target.value)
   }
 
   const SetMainData = () => {
+    if (!valx) {
+      alert('Please Select Type')
+      return
+    }
+    if (!date) {
+      alert('Please Select Date')
+      return
+    }
     console.log("Button Clicked", valx, amount)
-    if (valx === "incomes") {
-      setTotal(parseInt(income) + parseInt(expense))
+    if (valx === "INCOME") {
       setincome(parseInt(income) + parseInt(amount))
+      setTotal((e) => e + parseInt(amount))
     }
-    else if (valx === "expenses") {
+    else if (valx === "EXPENSE") {
       setexpense(parseInt(expense) - parseInt(amount))
-      setTotal(parseInt(income) + parseInt(expense))
+      setTotal((e) => e - parseInt(amount))
+
     }
 
-    setOBList({
-      amount: amount,
-      date: date,
-      valx: valx
-    })
-    setList([...list, OBlist]);
-    console.log(list)
+    const tempObj = {
+      amount,
+      date,
+      valx,
+    }
+    setList([...list, tempObj]);
+    // console.log(list)
   }
-
-
-
-
   return (
     <div className="App">
       <header className="App-header">
@@ -57,79 +57,106 @@ function App() {
       <div className="main-cont">
 
         <div className="bal">
-          <h1>Total Balance : {total}</h1>
+          <h1><span className="red">Total Balance </span> : {total}</h1>
         </div>
         <div className="show-bals">
           <div className="show-bals1">
             <h3 className="same">INCOME</h3>
-            <h2 className="same">{income}</h2>
+            <h2 className="same"><span className="green">{income}</span></h2>
           </div>
+
           <div className="show-bals2">
             <h3 className="same">EXPENSE</h3>
-            <h2 className="same">{expense}</h2>
+            <h2 className="same"><span className="red">{expense}</span></h2>
           </div>
         </div>
-        <h2>ADD NEW TRANSECTION</h2>
+        <h2>ADD NEW TRANSACTION</h2>
         <div className="line"></div>
 
         <div className="input-take">
 
           <div className="same2">
             <h3>Enter Amount</h3>
-            <input type="text" className="input" placeholder="Enter Amount .." onChange={(e) => { storeAmount(e) }} />
+            <input type="text"
+              className="input"
+              placeholder="Enter Amount .."
+              onChange={(e) => {
+                setAmount(e.target.value)
+              }} />
           </div>
           <div className="same2">
             <h3>Enter Date</h3>
-            <input type="date" className="input" placeholder="Enter Your Task .." onChange={(e) => { storeDate(e) }} />
+            <input type="date"
+              className="input"
+              placeholder="Enter Your Task .."
+              onChange={(e) => {
+                setDate(e.target.value)
+              }} />
           </div>
 
           <div className="same2">
             <h3>Enter Category</h3>
             <select className="input" onClick={(value) => { storevalue(value) }}>
-              <option value="" disabled>--Please choose </option>
-              <option value="incomes">INCOME</option>
-              <option value="expenses">EXPENSES</option>
+              <option value="" >--Please choose </option>
+              <option value="INCOME">INCOME</option>
+              <option value="EXPENSE">EXPENSES</option>
             </select>
           </div>
         </div>
-        <button className="button-9" onClick={() => SetMainData()}>ADD TRANSECTION</button>
+        <button className="button-9" onClick={() => SetMainData()}>ADD TRANSACTION</button>
       </div>
       <div className="render-list">
-        <h2>ALL TRANSECTION</h2>
+        <h2>TRANSACTION HISTORY</h2>
         <div className="line"></div>
         {
-          list?.map((item) => {
-            <div className="list-main">
-              <div className="same3">{item.date}</div>
-              <div className="same3">{item.amount}</div>
-              <div className="same3">{item.type}</div>
-            </div>
-          })
-        }
+          list?.map((item, index) => {
+            return (
+              <>
+                {index >= startIndex - 1 && index <= endIndex - 1 ? (
+                  <div className="list-main">
+                    <div className="same3" ><span className="same4">{item.date}</span></div>
+                    <div className="same3"><span className="same4"> $ {item.amount}</span></div>
+                    <div className="same3"><span className="same4">{item.valx}</span></div>
+                  </div>
+                ) : null}
+              </>
+            )
+          })}
+        <div
+          style={{
+            marginTop: 20,
+            display: 'flex',
+            width: '100vw',
+            justifyContent: 'space-around',
+            marginBottom: 20,
+          }}
+        >
+          <button
+            className="button-x"
+            onClick={() => {
+              if (list.length > endIndex) {
+                setStartIndex(startIndex + 10)
+                setEndIndex(endIndex + 10)
+              }
+            }}
+          >
+            Increase {startIndex}
+          </button>
+          <button
+            className="button-x"
+            onClick={() => {
+              if (startIndex >= 10) {
+                setStartIndex(startIndex - 10)
+                setEndIndex(endIndex - 10)
+              }
+            }}
+          >
+            Decrease {endIndex}
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
-
-// {
-//   list.map((item,index)=>{
-//     return(
-//       <div>
-//        {index}
-//       </div>
-//     )
-//   })  
-// }
-
-
-// {
-//   list.map((item) => {
-//     <div className="list-main">
-//       {/* <div className="same3">{item.date}</div> */}
-//       <div className="same3">{item.amount}</div>
-//       <div className="same3">{item.type}</div>
-//     </div>
-//   })
-// }
